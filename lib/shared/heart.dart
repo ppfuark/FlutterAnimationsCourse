d04ace53f1b59ecfd2d548ce9f07e3a6b4b24772
ppfuark <phfuark2007@gmail.com>
2026-02-26 16:54:10 -0300

    Explicit Animation[feat]: Heart
    Use AnimationController and AnimatedBuilder Widget

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
 
 class Heart extends StatefulWidget {
+  const Heart({super.key});
+
   @override
-  _HeartState createState() => _HeartState();
+  State<Heart> createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with TickerProviderStateMixin {
+  AnimationController? controller;
+  Animation<Color?>? colorAnimation;
+  bool isFav = false;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 250))
+          ..addStatusListener((state) {
+            if (state == AnimationStatus.completed) {
+              setState(() {
+                isFav = true;
+              });
+            } else {
+              setState(() {
+                isFav = false;
+              });
+            }
+          });
+
+    colorAnimation = ColorTween(
+      begin: Colors.grey.shade400,
+      end: Colors.redAccent,
+    ).animate(controller!);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller!.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      animation: controller!,
+      builder: (context, child) {
+        return IconButton(
+          icon: Icon(Icons.favorite, color: colorAnimation!.value, size: 30),
+          onPressed: () {
+            isFav ? controller!.reverse() : controller!.forward();
+          },
+        );
+      },
     );
   }
 }
