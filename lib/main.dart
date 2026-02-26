316344e6b7a724790114a0624371d45dc9356405
ppfuark <phfuark2007@gmail.com>
2026-02-26 15:21:44 -0300

    Implicity Animation[feat]: TweenAnimationBuilder
    Use a value reference with begin and end

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ninja Trips',
-      home: Sandbox(),
+      home: Home(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/shared/screen_title.dart
+++ lib/shared/screen_title.dart
@@ -7,12 +7,25 @@ class ScreenTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(
-        fontSize: 36,
-        color: Colors.white,
-        fontWeight: FontWeight.bold,
+    return TweenAnimationBuilder(
+      tween: Tween<double>(begin: 0, end: 1),
+      duration: Duration(milliseconds: 500),
+      builder: (context, value, child) {
+        return Opacity(
+          opacity: value,
+          child: Padding(
+            padding: EdgeInsets.only(top: value * 20),
+            child: child,
+          ),
+        );
+      },
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 36,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     );
   }
