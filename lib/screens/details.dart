19fdd9d33fa802d996cb049caeedbbdf74366e93
ppfuark <phfuark2007@gmail.com>
2026-02-26 16:01:11 -0300

    Implicity Animation[feat]: Hero
    use a unique tag

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -4,18 +4,19 @@ import 'package:animations/shared/heart.dart';
 
 class Details extends StatelessWidget {
   final Trip trip;
-  Details({required this.trip});
+  const Details({super.key, required this.trip});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0),
       extendBodyBehindAppBar: true,
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            ClipRRect(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          ClipRRect(
+            child: Hero(
+              tag: trip.img!,
               child: Image.asset(
                 'images/${trip.img}',
                 height: 360,
@@ -23,31 +24,31 @@ class Details extends StatelessWidget {
                 alignment: Alignment.topCenter,
               ),
             ),
-            SizedBox(height: 30),
-            ListTile(
-              title: Text(
-                trip.title!,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.grey[800],
-                ),
-              ),
-              subtitle: Text(
-                '${trip.nights} night stay for only \$${trip.price}',
-                style: TextStyle(letterSpacing: 1),
-              ),
-              trailing: Heart(),
-            ),
-            Padding(
-              padding: EdgeInsets.all(18),
-              child: Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec mattis nisi, at euismod enim. Nulla nibh ipsum, elementum at leo vel, tempus elementum velit. Cras a arcu non sapien tristique commodo. Maecenas vel tempor leo. Morbi ac arcu a sem maximus laoreet. Fusce ac congue nisi. Fusce eu justo sit amet massa ornare rhoncus. Ut sagittis malesuada nibh, sed finibus risus facilisis eu. Etiam lobortis, diam id bibendum efficitur, arcu elit pellentesque nunc, vulputate accumsan dui est ut purus. Integer ornare eget augue in posuere. Vivamus pharetra purus vel diam efficitur, non ornare tellus tristique. Nunc pharetra nulla libero, sed gravida turpis pharetra in. Morbi quis ligula eleifend diam semper consequat. Vivamus tincidunt ligula sem, ac hendrerit dui lobortis consectetur. Mauris nunc lorem, hendrerit volutpat volutpat quis, faucibus sed diam. Nam scelerisque massa sem, at blandit neque molestie vel.",
-                style: TextStyle(color: Colors.grey[600], height: 1.4),
+          ),
+          SizedBox(height: 30),
+          ListTile(
+            title: Text(
+              trip.title!,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.grey[800],
               ),
             ),
-          ],
-        ),
+            subtitle: Text(
+              '${trip.nights} night stay for only \$${trip.price}',
+              style: TextStyle(letterSpacing: 1),
+            ),
+            trailing: Heart(),
+          ),
+          Padding(
+            padding: EdgeInsets.all(18),
+            child: Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec mattis nisi, at euismod enim. Nulla nibh ipsum, elementum at leo vel, tempus elementum velit. Cras a arcu non sapien tristique commodo. Maecenas vel tempor leo. Morbi ac arcu a sem maximus laoreet. Fusce ac congue nisi. Fusce eu justo sit amet massa ornare rhoncus. Ut sagittis malesuada nibh, sed finibus risus facilisis eu. Etiam lobortis, diam id bibendum efficitur, arcu elit pellentesque nunc, vulputate accumsan dui est ut purus. Integer ornare eget augue in posuere. Vivamus pharetra purus vel diam efficitur, non ornare tellus tristique. Nunc pharetra nulla libero, sed gravida turpis pharetra in. Morbi quis ligula eleifend diam semper consequat. Vivamus tincidunt ligula sem, ac hendrerit dui lobortis consectetur. Mauris nunc lorem, hendrerit volutpat volutpat quis, faucibus sed diam. Nam scelerisque massa sem, at blandit neque molestie vel.",
+              style: TextStyle(color: Colors.grey[600], height: 1.4),
+            ),
+          ),
+        ],
       ),
     );
   }
